c1fafaa8f6338f8de33d13aa24369e47c284cc17
vfabiano <vfabiano@esgi.fr>
2022-05-24 15:06:54 +0200

    exercice 6 :  - création d'un tableau qui boucle sur toutes les statistiques  - ajout d'une statistique après 3 seconde

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,24 @@ import { statistique } from '../models/statistic'
 })
 export class AppComponent {
   title = 'Evaluation_Angular';
-  stat1 : statistique =  {
+  stat: statistique[] = [{
     titre: "Démographie Allemagne",
     valeur: "80000010",
     identifiant: "24a6fb60-7f7f-11ec-b9a5-e501194af5d7"
-  };
-  stat2 : statistique = {
+  }, {
     titre: "Démographie France",
     valeur: "65000000",
     identifiant: "c537efa0-7db6-11ec-8b1d-5d44059e886b"
+  }];
+
+  constructor() {
+    setTimeout(() => {
+      this.stat.push({
+        titre: "Démographie Espagne",
+        valeur: "49331076",
+        identifiant: "c537efa0-7db6-11ec-b9a5-e501194af5d7"
+      });
+    }, 3000)
   }
 }
+
