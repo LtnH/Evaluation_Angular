ac2efd47dc5057064264359c522ae7aa00308329
vfabiano <vfabiano@esgi.fr>
2022-05-24 12:39:41 +0200

    exercice 4 :  - Création de deux variables statistiques  - affichage du titre des statistiques

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { statistique } from '../models/statistic'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Evaluation_Angular';
+  stat1 : statistique =  {
+    titre: "Démographie Allemagne",
+    valeur: "80000010",
+    identifiant: "24a6fb60-7f7f-11ec-b9a5-e501194af5d7"
+  };
+  stat2 : statistique = {
+    titre: "Démographie France",
+    valeur: "65000000",
+    identifiant: "c537efa0-7db6-11ec-8b1d-5d44059e886b"
+  }
 }
